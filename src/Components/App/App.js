1cccb48f786e10c3ce0317b6e7fc646d5b7efdd5
vinciusb <65973642+vinciusb@users.noreply.github.com>
2021-08-31 13:34:43 -0300

    Gradient Button updated to use styled-components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,22 @@
+import React from 'react';
 import './App.css';
 import GradientButton from '../GradientButton/GradientButton';
 
 function App() {
-
-  return (
-    <div className="App">
-      <GradientButton text={ "Olá".toUpperCase() } font="Arial" fontSize={ 30 } color={ [255, 255, 255] }
-        gradStart={ [0, 255, 0] } gradEnd={ [0, 0, 255] } onClick={ () => {} } />
-    </div>
-  );
+    return (
+        <div className="App">
+            <GradientButton
+                onClick={() => { }}
+                text={'Olá'.toUpperCase()}
+                font="Arial"
+                fontSize={30}
+                color={[255, 255, 255]}
+                gradStart={[0, 255, 0]}
+                gradEnd={[0, 0, 255]}
+                className="Olaa-bt"
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/Components/GradientButton/GradientButton.js
+++ src/Components/GradientButton/GradientButton.js
@@ -1,37 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
 import PropTypes from 'prop-types';
-
-import { makeStyles } from '@material-ui/core';
-import { useState, useEffect } from 'react';
 
 const startRadius = 40;
 
-const buttonStyles = makeStyles({
-    GradientButton: {
-        background: props => `linear-gradient(30deg, rgb(${props.gradStart[0]}, ${props.gradStart[1]}, ${props.gradStart[2]}), rgb(${props.gradEnd[0]}, ${props.gradEnd[1]}, ${props.gradEnd[2]}))`,
-        color: props => `rgb(${props.color[0]}, ${props.color[1]}, ${props.color[2]})`,
-        width: 'fit-content',
-        minWidth: '70px',
-        padding: '13px 22px',
-        borderRadius: '7px',
-        fontWeight: 'bolder',
-        fontSize: props => `${props.fontSize}px`,
-        fontFamily: props => props.font,
-        boxSizing: 'border-box',
-        overflow: 'hidden',
-        position: 'relative',
-        '&:hover': {
-            cursor: 'pointer',
-        },
-        '& *': {
-            margin: '0px',
-            userSelect: 'none',
-            pointerEvents: 'none',
-        }
-    }
-});
+const Ripple = keyframes`
+    from {
+        opacity: 1,
+    },
+    to {
+        opacity: 0,
+    },
+`;
 
-function GradientButton(props) {
+const Styled = {
+    GradientButton: styled.div`
+        background: ${(props) => `linear-gradient(30deg,rgb(${props.gradStart[0]},${props.gradStart[1]},${props.gradStart[2]}), rgb(${props.gradEnd[0]}, ${props.gradEnd[1]}, ${props.gradEnd[2]}))`};
+        color: ${(props) => `rgb(${props.color[0]}, ${props.color[1]}, ${props.color[2]})`};
+        width: fit-content;
+        min-width: 70px;
+        padding: 13px 22px;
+        border-radius: 7px;
+        font-weight: bolder;
+        font-size: ${(props) => `${props.fontSize}px`};
+        font-family: ${(props) => props.font};
+        box-sizing: border-box;
+        overflow: hidden;
+        position: relative;
+        &:hover {
+            cursor: pointer;
+        }
+        & * {
+            margin: 0px;
+            user-select: none;
+            pointer-events: none;
+        }
+    `,
+    Circle: styled.div`
+        width: ${(props) => `${2 * props.radius}px`};
+        height: ${(props) => `${2 * props.radius}px`};
+        left: ${(props) => `${props.offsets.left}px`};
+        top: ${(props) => `${props.offsets.top}px`};
+        background-color: rgb(255,255,255,0.4);
+        position: absolute;
+        border-radius: 50%;
+        animation: ${Ripple} 0.5s ease-out forwards;
+        transition: width 0.5s ease-out,
+                    height 0.5s ease-out,
+                    left 0.5s ease-out,
+                    top 0.5s ease-out;
+    `,
+};
+
+function GradientButton({
+    onClick, text, font, fontSize, color, gradStart, gradEnd, className,
+}) {
     const [clickInfo, setClickInfo] = useState();
     const [aniIsRunning, setAniIsRunning] = useState(false);
 
@@ -42,68 +65,61 @@ function GradientButton(props) {
         const clickY = e.clientY - rect.top;
 
         //  Obtem maior distancia do eixo x
-        var growX, growY;
+        let growX; let
+            growY;
         if(clickX < rect.width / 2) growX = rect.width - clickX;
         else growX = clickX;
 
         //  Obtem maior distancia do eixo y
         if(clickY < rect.height / 2) growY = rect.height - clickY;
         else growY = clickY;
-    
-        const farthestCorner = Math.sqrt(Math.pow(growX, 2) + Math.pow(growY, 2));
 
-        setClickInfo({ left: clickX , top: clickY, farCorner: farthestCorner });
+        const farthestCorner = Math.sqrt(growX ** 2 + growY ** 2);
+
+        setClickInfo({ left: clickX, top: clickY, farCorner: farthestCorner });
         setAniIsRunning(true);
         setTimeout(() => { setAniIsRunning(false); }, 600);
 
         // Faz ação proposta para o click
-        props.onClick();
+        onClick();
     };
 
-    const classes = buttonStyles({ ...props });
-    const className = `${props.className} ${classes.GradientButton}`
     return (
-        <div className={ className } onClick={ handleClick }>
-            <p>{ props.text }</p>
-            { aniIsRunning && <RippleDiv clickPoint={ clickInfo } /> }
-        </div>
+        <Styled.GradientButton
+            onClick={handleClick}
+            font={font}
+            fontSize={fontSize}
+            color={color}
+            gradStart={gradStart}
+            gradEnd={gradEnd}
+            className={className}
+        >
+            <p>{ text }</p>
+            { aniIsRunning && <RippleDiv clickPoint={clickInfo} /> }
+        </Styled.GradientButton>
     );
 }
 
-const rippleStyles = makeStyles({
-    Circle: {
-        width: props => `${2 * props.circleRadius}px`,
-        height: props => `${2 * props.circleRadius}px`,
-        left: props => `${ props.circleOffsets.left }px`,
-        top: props => `${ props.circleOffsets.top }px`,
-        backgroundColor: 'rgb(255,255,255,0.4)',
-        position: 'absolute',
-        borderRadius: '50%',
-        animation: '$ripple 0.5s ease-out forwards',
-        transition: 'width 0.5s ease-out, height 0.5s ease-out, left 0.5s ease-out, top 0.5s ease-out',
-    },
-    '@keyframes ripple': {
-        'from': {
-            opacity: '1'
-        },
-        'to': {
-            opacity: '0',
-        }
-    },
-});
-
-function RippleDiv(props) {
+function RippleDiv({ clickPoint }) {
     const [circleRadius, setCircleRadius] = useState(startRadius);
-    const [circleOffsets, setCircleOffsets] = useState({ left: props.clickPoint.left - startRadius, top: props.clickPoint.top - startRadius });
+    const [circleOffsets, setCircleOffsets] = useState(
+        {
+            left: clickPoint.left - startRadius,
+            top: clickPoint.top - startRadius,
+        },
+    );
 
     useEffect(() => {
-        setCircleRadius(props.clickPoint.farCorner);
-        setCircleOffsets({ left: props.clickPoint.left - props.clickPoint.farCorner, top: props.clickPoint.top - props.clickPoint.farCorner });
-    }, [props]);
+        setCircleRadius(clickPoint.farCorner);
+        setCircleOffsets({
+            left: clickPoint.left - clickPoint.farCorner,
+            top: clickPoint.top - clickPoint.farCorner,
+        });
+    }, [clickPoint]);
 
-    const classes = rippleStyles({ ...props, circleRadius, circleOffsets });
+    // const classes = rippleStyles({ ...props, circleRadius, circleOffsets });
     return (
-        <div className={ classes.Circle }></div>
+        <Styled.Circle radius={circleRadius} offsets={circleOffsets} />
     );
 }
 
@@ -120,11 +136,15 @@ GradientButton.propTypes = {
     onClick: PropTypes.func,
     text: PropTypes.string.isRequired,
     font: PropTypes.string,
-    className: PropTypes.string,
     fontSize: PropTypes.number.isRequired,
-    color: PropTypes.array,
-    gradStart: PropTypes.array,
-    gradEnd: PropTypes.array,
+    color: PropTypes.objectOf,
+    gradStart: PropTypes.objectOf,
+    gradEnd: PropTypes.objectOf,
+    className: PropTypes.string,
+};
+
+RippleDiv.propTypes = {
+    clickPoint: PropTypes.objectOf.isRequired,
 };
 
 export default GradientButton;
